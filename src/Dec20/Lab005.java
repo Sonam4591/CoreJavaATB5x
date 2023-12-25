cf3cb79753829b154c5c99a53e444a2b4560ba71
Sonam4591 <sonamnuri4@gmail.com>
2023-12-25 16:47:12 +0530

    i want the latest changes

---
+++ src/Dec20/Lab005.java
@@ -0,0 +1,21 @@
+package src.Dec20;
+
+public class Lab005 {
+
+    public static void main(String[] args)
+    {
+        // Why we are using this?
+        // Formatting purpose
+        String name = "Sonam"; // Bunch of Char
+        // Concate +
+        System.out.println("Your name is -> " + name);
+
+        // Formatting ( printf)
+        System.out.printf("Your name is -> %s",name);
+
+        System.out.println();
+
+        float pi = 3.14159f;
+        System.out.printf("Your value is  %f",pi);
+    }
+}
