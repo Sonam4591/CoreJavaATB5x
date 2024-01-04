cb01c0eee2f29689be9475f41ecb0504c3f82534
Sonam4591 <sonamnuri4@gmail.com>
2024-01-04 17:37:38 +0530

    oops

--- src/Dec25/Lab110.java
+++ src/Dec25/Lab110.java
@@ -3,8 +3,10 @@ package src.Dec25;
 public class Lab110 {
     public static void main(String[] args) {
         // 1D
-        int a[]={3};
+        int a[]={1,2,3};
+
         // 2D
+
 
     }
 }
---
+++ src/Oops/Jan4/Lab112.java
@@ -0,0 +1,46 @@
+package src.Oops.Jan4;
+
+public class Lab112 {
+    public static void main(String[] args) {
+
+        int c= add_1(); //no args
+        int c1= add_1(5,9); // 2 args  //Definition of the function
+        int c2= add_1(55); // 2 args
+        System.out.println(c);
+        System.out.println(c1);
+        System.out.println(c2);
+
+
+        sayhello(); // call to the function
+
+        //        int a,b,c;
+//        a=10;
+//        b=20;
+//        c=a+b;
+//        System.out.println("Addition is : "+c);
+//
+//        a=54;
+//        b=68;
+//        int d =a+b;
+//        System.out.println(c);
+    }
+
+  static int add_1(int a, int b)
+    {
+        return a+b;
+    }
+    static int add_1(int a)
+    {
+        return a+a;
+    }
+
+    static int add_1()
+    {
+        return 99;
+    }
+
+   static void sayhello()
+    {
+        System.out.println("hi...");
+    }
+}
