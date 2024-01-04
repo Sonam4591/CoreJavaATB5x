479db4601a32c977f9a68706b21a4920e5cce087
Sonam4591 <sonamnuri4@gmail.com>
2024-01-04 19:08:40 +0530

    Main Method overloading example

---
+++ src/Oops/Jan4/Lab116.java
@@ -0,0 +1,26 @@
+package src.Oops.Jan4;
+
+import java.util.Scanner;
+
+public class Lab116 {
+
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Enter your NAME : ");
+        System.out.println(scanner.next());
+    }
+    public static void main(int a) {
+        System.out.println("ANOTHER MAIN");
+    }
+
+    public static void main(int a, int b)
+    {
+        System.out.println("one more main");
+    }
+
+    public static int main(int a, int b, int c)
+    {
+        System.out.println("one more main");
+        return 99;
+    }
+}
